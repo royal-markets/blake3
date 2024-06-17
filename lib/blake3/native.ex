4aae41f27d97402f8b5f60672900adde30c2d380
Sonny Scroggin <sonny@scrogg.in>
2024-06-17 12:47:58 -0500

    Rustler Precompiled

--- lib/blake3/native.ex
+++ lib/blake3/native.ex
@@ -3,9 +3,30 @@ defmodule Blake3.Native do
   Blake3.Native is the rustler module that will be replaced with the nif rust functions.
   This module doesn't need to be called direcly.
   """
+  config = Mix.Project.config()
+  version = config[:version]
 
-  use Rustler,
-    otp_app: :blake3
+  force_build = Application.compile_env(:rustler_precompiled, :force_build, [])
+  source_url = Application.compile_env(:blake3, :source_url, config[:source_url])
+
+  use RustlerPrecompiled,
+    otp_app: :blake3,
+    crate: "blake3",
+    base_url: "#{source_url}/releases/download/v#{version}",
+    force_build: System.get_env("BLAKE3_BUILD") in ["1", "true"] or force_build[:blake3],
+    nif_versions: ["2.15"],
+    targets: [
+      "aarch64-apple-darwin",
+      "aarch64-unknown-linux-gnu",
+      "aarch64-unknown-linux-musl",
+      "arm-unknown-linux-gnueabihf",
+      "x86_64-apple-darwin",
+      "x86_64-pc-windows-gnu",
+      "x86_64-pc-windows-msvc",
+      "x86_64-unknown-linux-gnu",
+      "x86_64-unknown-linux-musl"
+    ],
+    version: version
 
   def hash(_str), do: error()
   def new(), do: error()
--- mix.exs
+++ mix.exs
@@ -1,16 +1,23 @@
 defmodule MixBlake3.Project do
   use Mix.Project
 
+  @source_url "https://github.com/Thomas-Jean/blake3"
+  @version "1.0.2"
+
+  @rayon Application.compile_env(:blake3, :rayon, System.get_env("BLAKE3_RAYON"))
+  @simd_mode Application.compile_env(:blake3, :simd_mode, System.get_env("BLAKE3_SIMD_MODE"))
+
   def project do
     [
       app: :blake3,
-      version: "1.0.2",
+      version: @version,
       elixir: "~> 1.13",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
-      package: package()
+      package: package(),
+      source_url: @source_url
     ]
   end
 
@@ -21,15 +28,24 @@ defmodule MixBlake3.Project do
   defp package() do
     [
       description: "Elixir binding for the Rust Blake3 implementation",
-      files: ["lib", "native", ".formatter.exs", "README*", "LICENSE*", "mix.exs"],
+      files: [
+        "lib",
+        "native",
+        ".formatter.exs",
+        "README*",
+        "LICENSE*",
+        "mix.exs",
+        "checksum-*.exs"
+      ],
       licenses: ["Apache-2.0"],
-      links: %{"GitHub" => "https://github.com/Thomas-Jean/blake3"}
+      links: %{"GitHub" => @source_url}
     ]
   end
 
   defp deps do
     [
-      {:rustler, "~> 0.30"},
+      {:rustler, "~> 0.30", optional: true},
+      {:rustler_precompiled, "~> 0.7"},
       {:ex_doc, "~> 0.21", only: [:dev, :test], runtime: false}
     ]
   end
@@ -38,13 +54,13 @@ defmodule MixBlake3.Project do
     [
       extras: ["README.md"],
       main: "readme",
-      source_url: "https://github.com/Thomas-Jean/blake3"
+      source_url: @source_url
     ]
   end
 
   def config_features() do
     simd =
-      case Application.get_env(:blake3, :simd_mode) || System.get_env("BLAKE3_SIMD_MODE") do
+      case @simd_mode do
         "c_neon" -> "neon"
         :c_neon -> "neon"
         "neon" -> "neon"
@@ -52,12 +68,7 @@ defmodule MixBlake3.Project do
         _ -> nil
       end
 
-    rayon =
-      if !is_nil(Application.get_env(:blake3, :rayon) || System.get_env("BLAKE3_RAYON")) do
-        "rayon"
-      else
-        nil
-      end
+    rayon = if @rayon, do: "rayon", else: nil
 
     Enum.reject([simd, rayon], &is_nil/1)
   end
