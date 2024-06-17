b64c203b5bd866ab6c43eaeb8ec6dd9b749999e8
Sonny Scroggin <sonny@scrogg.in>
2024-06-17 13:20:27 -0500

    Add checksum

---
+++ checksum-Elixir.Blake3.Native.exs
@@ -0,0 +1,11 @@
+%{
+  "blake3-v1.0.3-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:fa25a68693a0824c299ab0717bc46849272c0681b863fb3494c31e2871b29169",
+  "blake3-v1.0.3-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:e8c595c68d021e66ef07bc5468a205764bf84c45a14c02762fa503bd87055fc4",
+  "libblake3-v1.0.3-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:cc48c5d41911a6e947b36b12f5345e4c07ee273ca1389eaa0d67970095684276",
+  "libblake3-v1.0.3-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:0907ed122301b1acfe0c6a2de68445a7df60adf27d3444638b87026795935a1a",
+  "libblake3-v1.0.3-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:459998cb86bf18207f2ac5fce17db528cec1d842cd8f73aaeac7a62e9ca86dc3",
+  "libblake3-v1.0.3-nif-2.15-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:69e2aca5a84f779938065420a9196bbebf2fb5c7567cc31c999ec9c9694fe587",
+  "libblake3-v1.0.3-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:1e4278e95d1b418ac0deb892fae4c4bbc938fd6b41e658c67e4c53fae0dc422c",
+  "libblake3-v1.0.3-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:ded52eadc26c3f46d456419c1f1186da051483f26f3bb9ae734b7ec75a7dbb8f",
+  "libblake3-v1.0.3-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:3506cd9aefee8b28b1f33e8c3464343974c63aa8542ccc1853924f315f1726df",
+}
--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 import Config
 
+config :blake3, source_url: "https://github.com/royal-markets/blake3"
+
 config :blake3, Blake3.Native,
   mode: :release,
   features: MixBlake3.Project.config_features()
